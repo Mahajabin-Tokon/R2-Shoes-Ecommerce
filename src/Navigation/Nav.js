636be34b5c2cf4cd3ca1c9aa24a8620962d5a275
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-07-03 21:27:35 -0600

    update

---
+++ src/Navigation/Nav.js
@@ -0,0 +1,20 @@
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <nav>
+      <div className="nav-container">
+        <input type="text" className="search-input" placeholder="Search" />
+      </div>
+
+      <div className="profile-container">
+        <a href="#"></a>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
