a7659bed7b1b032a68a95655a6725debd562888d
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-07-22 18:32:09 -0600

    Started working on filter functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import Nav from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
 
 function App() {
+  // Declaring all states in App; Using App component as a kind of store for all the states
+  const [selectedCategory, useSelectedCategory] = useState(null);
+  
+  // Input Filter
   return (
     <>
       <Sidebar />
